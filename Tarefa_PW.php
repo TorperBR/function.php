e700b27d5ef1eb7b23fd6972042fa83b8c7c1870
TorperBR <104833003+TorperBR@users.noreply.github.com>
2022-05-03 08:22:45 -0300

    Add files via upload

---
+++ Tarefa_PW.php
@@ -0,0 +1,54 @@
+<?php
+    echo "SOMA";
+    echo "<br>";
+    
+    function soma($valor1, $valor2){
+        $resultado  = $valor1 + $valor2;
+        return $resultado;
+    }
+    
+    $valor =  soma(1000, 2000);
+    echo $valor;
+
+    echo "<hr>";
+
+
+    echo "SUBTRAÇÃO";
+    echo "<br>";
+    
+    function subtracao($valor1, $valor2){
+        $resultado  = $valor1 + $valor2;
+        return $resultado;
+    }
+    
+    $valor =  subtracao(1000, 2000);
+    echo $valor;
+
+    echo "<hr>";
+    
+
+    echo "DIVISÃO";
+    echo "<br>";
+    
+    function divisao($valor1, $valor2){
+        $resultado  = $valor1 + $valor2;
+        return $resultado;
+    }
+    
+    $valor =  divisao(1000, 2000);
+    echo $valor;
+
+    echo "<hr>";
+
+    echo "MULTIPLICAÇÃO";
+    echo "<br>";
+    
+    function multiplicacao($valor1, $valor2){
+        $resultado  = $valor1 + $valor2;
+        return $resultado;
+    }
+    
+    $valor =  multiplicacao(1000, 2000);
+    echo $valor;
+
+?>
